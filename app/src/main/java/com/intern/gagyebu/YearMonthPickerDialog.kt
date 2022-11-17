55eae26bfd280ec1a2f765f8491242b3c4ca6f37
jun34723 <jun34723@avatye.com>
2022-11-17 18:29:16 +0900

    feat: INTERN-103 년,월 입력받는 datePicker 추가

---
+++ app/src/main/java/com/intern/gagyebu/YearMonthPickerDialog.kt
@@ -0,0 +1,53 @@
+package com.intern.gagyebu
+
+import android.app.AlertDialog
+import android.app.DatePickerDialog.OnDateSetListener
+import android.app.Dialog
+import android.os.Bundle
+import android.widget.NumberPicker
+import androidx.fragment.app.DialogFragment
+import com.intern.gagyebu.databinding.YearMonthPickerBinding
+import java.util.*
+
+
+class YearMonthPickerDialog : DialogFragment() {
+
+    var customListener: OnDateSetListener? = null
+    var cal: Calendar = Calendar.getInstance()
+
+    fun setListener(listener: OnDateSetListener?) {
+        this.customListener = listener
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val builder = AlertDialog.Builder(activity)
+        val binding = YearMonthPickerBinding.inflate(requireActivity().layoutInflater)
+
+        val monthPicker = binding.pickerMonth as NumberPicker
+        val yearPicker = binding.pickerYear as NumberPicker
+        binding.btnCancel.setOnClickListener {
+            this@YearMonthPickerDialog.dialog?.cancel()
+        }
+        binding.btnConfirm.setOnClickListener {
+            customListener!!.onDateSet(null, yearPicker.value, monthPicker.value, 0)
+            this@YearMonthPickerDialog.dialog?.cancel()
+        }
+        monthPicker.minValue = 1
+        monthPicker.maxValue = 12
+        monthPicker.value = cal.get(Calendar.MONTH) + 1
+        val year: Int = cal.get(Calendar.YEAR)
+        yearPicker.minValue = MIN_YEAR
+        yearPicker.maxValue = MAX_YEAR
+        yearPicker.value = year
+        builder.setView(binding.root) // Add action buttons
+
+        return builder.create()
+    }
+
+    companion object {
+        private const val MAX_YEAR = 2099
+        private const val MIN_YEAR = 1980
+    }
+
+
+}
--- app/src/main/java/com/intern/gagyebu/main/MainActivity.kt
+++ app/src/main/java/com/intern/gagyebu/main/MainActivity.kt
@@ -1,51 +1,50 @@
 package com.intern.gagyebu.main
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.intern.gagyebu.add.AddItemActivity
 import com.intern.gagyebu.App
+import com.intern.gagyebu.YearMonthPickerDialog
 import com.intern.gagyebu.databinding.ActivityMainBinding
 import com.intern.gagyebu.room.AppDatabase
 import com.intern.gagyebu.room.ItemRepo
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     private val database = AppDatabase.getDatabase(App.context())
     private val factory = MainViewModelFactory(ItemRepo(database.itemDao()))
 
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         val viewModel = ViewModelProvider(this, factory)[MainViewModel::class.java]
 
-        viewModel.resetOrder()
+        viewModel.initValue()
 
-        binding.recyclerview.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        viewModel.incomeValue.observe(this) {
+            binding.income.text = it.toString()
+        }
+
+        viewModel.spendValue.observe(this) {
+            binding.spend.text = it.toString()
+        }
+
+        binding.id.setOnClickListener{
+            val datePicker = YearMonthPickerDialog()
+            datePicker.setListener { _, year, month, _ ->
+                Log.d("YearMonthPickerTest", year.toString() + month.toString())
+            }
+            datePicker.show(supportFragmentManager, "DatePicker")
+        }
+
+        binding.recyclerview.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         val adapter = Adapter()
         binding.recyclerview.adapter = adapter
         subscribeUi(adapter, viewModel)
-
-        binding.add.setOnClickListener{
-            startActivity(Intent(this, AddItemActivity::class.java))
-        }
-
-        binding.delete.setOnClickListener{
-            CoroutineScope(Dispatchers.IO).launch {
-                database.itemDao()
-                    .deleteItem(binding.optionNumber.text.toString().toInt())
-            }
-        }
-
-        binding.order.setOnClickListener{
-            viewModel.setOrder(binding.optionNumber.text.toString().toInt())
-        }
     }
 
     private fun subscribeUi(adapter: Adapter, viewModel: MainViewModel) {
