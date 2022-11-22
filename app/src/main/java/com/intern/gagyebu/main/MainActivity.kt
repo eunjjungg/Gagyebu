9f1dba8c8c505d2c2b3a934162a1ed7f498ee279
jun34723 <jun34723@avatye.com>
2022-11-22 16:09:47 +0900

    refactor: INTERN-103 아키텍처 패턴 수정

--- app/src/main/java/com/intern/gagyebu/main/MainActivity.kt
+++ app/src/main/java/com/intern/gagyebu/main/MainActivity.kt
@@ -1,16 +1,12 @@
 package com.intern.gagyebu.main
 
-import android.app.DatePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.intern.gagyebu.*
-import com.intern.gagyebu.add.AddActivutyViewModel
 import com.intern.gagyebu.add.AddItemActivity
 import com.intern.gagyebu.databinding.ActivityMainBinding
 import com.intern.gagyebu.dialog.OptionDialogListener
@@ -18,9 +14,6 @@ import com.intern.gagyebu.dialog.OptionSelectDialog
 import com.intern.gagyebu.dialog.YearMonthPickerDialog
 import com.intern.gagyebu.room.AppDatabase
 import com.intern.gagyebu.room.ItemRepo
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -28,49 +21,48 @@ class MainActivity : AppCompatActivity() {
     private val database = AppDatabase.getDatabase(App.context())
     private val factory = MainViewModelFactory(ItemRepo(database.itemDao()))
 
+    companion object {
+        val calendar: Calendar = Calendar.getInstance()
+        val YEAR = calendar.get(Calendar.YEAR)
+        val MONTH = calendar.get(Calendar.MONTH) + 1
+        val DATE = calendar.get(Calendar.DATE)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         val viewModel = ViewModelProvider(this, factory)[MainViewModel::class.java]
         binding.mainViewModel = viewModel
-        val calendar: Calendar = Calendar.getInstance()
 
-        val itemGetOption = ItemGetOption(
-            calendar.get(Calendar.YEAR),
-            calendar.get(Calendar.MONTH) + 1,
-            "all",
-            "date"
-        )
-        viewModel.setData(itemGetOption)
+        binding.calender.text = getString(R.string.show_date, YEAR, MONTH)
 
         //해당 년/월의 수입 observing
         viewModel.incomeValue.observe(this) {
-            binding.income.text = it.toString() + " 원"
+            binding.income.text = getString(R.string.show_won, "$it")
         }
 
         //해당 년/월의 지출 observing
         viewModel.spendValue.observe(this) {
-            binding.spend.text = it.toString() + " 원"
+            binding.spend.text = getString(R.string.show_won, "$it")
         }
 
         //해당 년/월의 총합 observing
         viewModel.totalValue.observe(this) {
-            binding.total.text = it.toString() + " 원"
+            binding.total.text = getString(R.string.show_won, "$it")
         }
 
+        //저장버튼 (test)
         binding.save.setOnClickListener {
             startActivity(Intent(this, AddItemActivity::class.java))
         }
 
         //달력 다이얼로그
-        binding.id.setOnClickListener {
+        binding.calender.setOnClickListener {
             val datePicker = YearMonthPickerDialog()
             datePicker.setListener { _, year, month, _ ->
-                itemGetOption.year = year
-                itemGetOption.month = month
-                viewModel.setData(itemGetOption)
+                viewModel.changeData(year, month)
+                binding.calender.text = getString(R.string.show_date, year, month)
             }
-
             datePicker.show(supportFragmentManager, "DatePicker")
         }
 
@@ -79,12 +71,9 @@ class MainActivity : AppCompatActivity() {
             val optionPicker = OptionSelectDialog()
             optionPicker.setListener(object : OptionDialogListener {
                 override fun option(filter: String, order: String) {
-                    itemGetOption.filter = filter
-                    itemGetOption.order = order
-                    viewModel.setData(itemGetOption)
+                    viewModel.changeOption(filter, order)
                 }
             })
-
             optionPicker.show(supportFragmentManager, "OptionPicker")
         }
 
@@ -111,3 +100,4 @@ class MainViewModelFactory(
     @Suppress("UNCHECKED_CAST")
     override fun <T : ViewModel> create(modelClass: Class<T>) = MainViewModel(repository) as T
 }
+
