a948a7fabc5654eec5a14efec5379520e1f17c89
jun34723 <jun34723@avatye.com>
2022-11-24 17:39:11 +0900

    feat: INTERN-103 ComposeUI 정리, 예외처리 로직 추가

--- app/src/main/java/com/intern/gagyebu/main/MainActivity.kt
+++ app/src/main/java/com/intern/gagyebu/main/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.intern.gagyebu.*
 import com.intern.gagyebu.add.AddItemActivity
-import com.intern.gagyebu.add.AddItemActivity2
 import com.intern.gagyebu.databinding.ActivityMainBinding
 import com.intern.gagyebu.dialog.OptionDialogListener
 import com.intern.gagyebu.dialog.OptionSelectDialog
@@ -26,7 +25,6 @@ class MainActivity : AppCompatActivity() {
         private val calendar: Calendar = Calendar.getInstance()
         val YEAR = calendar.get(Calendar.YEAR)
         val MONTH = calendar.get(Calendar.MONTH) + 1
-        val DATE = calendar.get(Calendar.DATE)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -52,9 +50,14 @@ class MainActivity : AppCompatActivity() {
             binding.total.text = getString(R.string.show_won, "$it")
         }
 
+        //달력 text observing
+        viewModel.date.observe(this) {
+            binding.calender.text = it
+        }
+
         //저장버튼 (test)
         binding.save.setOnClickListener {
-            startActivity(Intent(this, AddItemActivity2::class.java))
+            startActivity(Intent(this, AddItemActivity::class.java))
         }
 
         //달력 다이얼로그
@@ -62,7 +65,7 @@ class MainActivity : AppCompatActivity() {
             val datePicker = YearMonthPickerDialog()
             datePicker.setListener { _, year, month, _ ->
                 viewModel.changeData(year, month)
-                binding.calender.text = getString(R.string.show_date, year, month)
+                viewModel.updateDate(getString(R.string.show_date, year, month))
             }
             datePicker.show(supportFragmentManager, "DatePicker")
         }
