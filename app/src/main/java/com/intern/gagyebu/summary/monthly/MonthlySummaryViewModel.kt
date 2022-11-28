355aa6df84363e58704e317351bd328703b66625
jung eunjung <agjjung@avatye.com>
2022-11-28 16:14:57 +0900

    feat: INTERN-89, 소수점 단위 수정

--- app/src/main/java/com/intern/gagyebu/summary/monthly/MonthlySummaryViewModel.kt
+++ app/src/main/java/com/intern/gagyebu/summary/monthly/MonthlySummaryViewModel.kt
@@ -35,16 +35,21 @@ class MonthlySummaryViewModel(private val itemRepository: ItemRepository): ViewM
         val _data = if(data.size > 5)
             data.subList(0, 5)
         else data
+
         var sumOfEachAccount = 0
+
         for(i in _data.indices)
             sumOfEachAccount += _data[i].sum
 
+        //각 부분이 차지하는 %를 구하여 PieElement에 할당
         val pieElementList = mutableListOf<PieElement>()
         for(i in _data.indices)
             pieElementList.add(
-                PieElement(_data[i].category, _data[i].sum.toFloat() / sumOfEachAccount)
+                PieElement(_data[i].category,
+                    Math.round(_data[i].sum.toFloat() / sumOfEachAccount * 100) / 100f)
             )
 
+        //각 합이 총 100%를 이루는지 확인
         var checkOne: Float = 0f
         for(i in pieElementList.indices)
             checkOne += pieElementList[i].percentage
@@ -55,8 +60,9 @@ class MonthlySummaryViewModel(private val itemRepository: ItemRepository): ViewM
                 removeAt(pieElementList.lastIndex)
                 add(PieElement(tmp.name, tmp.percentage + (1f - checkOne)))
             }
-
         }
+
+        //라이브 데이터 적용
         pieChartData.value = pieElementList
     }
 
