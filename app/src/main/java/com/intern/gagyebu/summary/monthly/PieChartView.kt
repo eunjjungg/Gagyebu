b6e3db0003af5342ef2025cd1644f2a63c5659c6
jung eunjung <agjjung@avatye.com>
2022-11-29 14:07:37 +0900

    comment: INTERN-130 PieChartView.kt 주석 추가

--- app/src/main/java/com/intern/gagyebu/summary/monthly/PieChartView.kt
+++ app/src/main/java/com/intern/gagyebu/summary/monthly/PieChartView.kt
@@ -6,7 +6,6 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.RectF
 import android.util.AttributeSet
-import android.util.Log
 import android.view.View
 import android.view.animation.LinearInterpolator
 import androidx.core.animation.addListener
@@ -19,7 +18,6 @@ class PieChartView: View {
         initPaint()
     }
     constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
-        initAttribute(attrs)
         initPaint()
     }
 
@@ -33,29 +31,7 @@ class PieChartView: View {
     private val eachPieStartAngle = mutableListOf<Float>(-90f, 0f, 0f, 0f, 0f)
     private val eachPieProgess = mutableListOf<Float>(0f, 0f, 0f, 0f, 0f)
 
-
-    private fun initAttribute(attributeSet: AttributeSet?) {
-        if (attributeSet == null) {
-            return
-        }
-
-        val attrs = context.theme.obtainStyledAttributes(
-            attributeSet,
-            R.styleable.PieChartView,
-            0,
-            0
-        )
-
-        try {
-
-
-        } finally {
-            attrs.recycle()
-        }
-
-
-    }
-
+    //Paint 객체 각각 초기화
     private fun initPaint() {
         val setPaintingLambda: (Int) -> Paint = {
             Paint().apply {
@@ -77,6 +53,8 @@ class PieChartView: View {
         }
     }
 
+    //틀만 그려두고 drawArc의 각 시작점과 그리는 각도는
+    //애니메이션을 진행하면서 정함
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
 
@@ -124,16 +102,18 @@ class PieChartView: View {
         )
     }
 
-    fun startAnimator() {
+    //각 파이에 해당하는 애니메이터들을 정의해주고
+    //첫 번째 파이를 그리는 valAnim0부터 start()하여 valAnim0 -> valAnim4까지 차례로 부르며 파이를 그림.
+    private fun startAnimator() {
         val baseValueAnimator = BaseValueAnimator()
 
-        val valAnim4 = baseValueAnimator.makeBaseValueAnimaotor(eachPieRangeList[4]).apply {
+        val valAnim4 = baseValueAnimator.makeBaseValueAnimator(eachPieRangeList[4]).apply {
             addUpdateListener {
                 eachPieProgess[4] = it.animatedValue as Float
                 invalidate()
             }
         }
-        val valAnim3 = baseValueAnimator.makeBaseValueAnimaotor(eachPieRangeList[3]).apply {
+        val valAnim3 = baseValueAnimator.makeBaseValueAnimator(eachPieRangeList[3]).apply {
             addUpdateListener {
                 eachPieProgess[3] = it.animatedValue as Float
                 invalidate()
@@ -142,7 +122,7 @@ class PieChartView: View {
                 valAnim4.start()
             })
         }
-        val valAnim2 = baseValueAnimator.makeBaseValueAnimaotor(eachPieRangeList[2]).apply {
+        val valAnim2 = baseValueAnimator.makeBaseValueAnimator(eachPieRangeList[2]).apply {
             addUpdateListener {
                 eachPieProgess[2] = it.animatedValue as Float
                 invalidate()
@@ -151,7 +131,7 @@ class PieChartView: View {
                 valAnim3.start()
             })
         }
-        val valAnim1 = baseValueAnimator.makeBaseValueAnimaotor(eachPieRangeList[1]).apply {
+        val valAnim1 = baseValueAnimator.makeBaseValueAnimator(eachPieRangeList[1]).apply {
             addUpdateListener {
                 eachPieProgess[1] = it.animatedValue as Float
                 invalidate()
@@ -160,7 +140,7 @@ class PieChartView: View {
                 valAnim2.start()
             })
         }
-        val valAnim0 = baseValueAnimator.makeBaseValueAnimaotor(eachPieRangeList[0]).apply {
+        val valAnim0 = baseValueAnimator.makeBaseValueAnimator(eachPieRangeList[0]).apply {
             addUpdateListener {
                 eachPieProgess[0] = it.animatedValue as Float
                 invalidate()
@@ -171,14 +151,12 @@ class PieChartView: View {
         }
 
         valAnim0.start()
-
-
-
     }
 
+    //중복되는 animator 생성 부분을 inner class로 뺌.
     inner class BaseValueAnimator {
         private val second1 = 1000L
-        fun makeBaseValueAnimaotor(range: Float): ValueAnimator {
+        fun makeBaseValueAnimator(range: Float): ValueAnimator {
             return ValueAnimator.ofFloat(0f, range).apply {
                 duration = (animationDuration * second1 * range / 360f).toLong()
                 interpolator = LinearInterpolator()
@@ -189,13 +167,9 @@ class PieChartView: View {
         }
     }
 
-
-    /**
-     * 여기서 문제 발생!! 360을 넘는 상황이 발생
-     */
-
+    //뷰에서 이 커스텀 뷰를 설정하는 함수
+    //뷰에서는 setPercentage를 통해 각 파이 원소들의 이름과 float을 정의해둔 elementList를 함께 전달
     fun setPercentage(elementList: List<PieElement>){
-
         val _elementList = if(elementList.size > 5)
             elementList.subList(0, 5)
         else elementList
@@ -214,7 +188,10 @@ class PieChartView: View {
                 eachPieStartAngle[i] = eachPieStartAngle[i - 1] + eachPieRangeList[i - 1]
             }
         }
-        //보정을 해주는 부분
+
+        //소숫점 보정을 해주는 부분.
+        //각 pie의 rangeList의 총합이 359, 361일 경우 마지막 pie의 값을 조절해서
+        //총 pie의 rangeList의 총합이 360이 되도록 추가적으로 보정을 해주는 부분
         var check360 = 0f
         for(i in eachPieRangeList.indices) {
             check360 += eachPieRangeList[i]
@@ -223,10 +200,7 @@ class PieChartView: View {
             eachPieRangeList[_elementList.lastIndex] += 360f - check360
         }
 
-
-
-
-
+        //보정 완료 후 애니메이터들을 정의해주는 startAnimator() 호출
         startAnimator()
     }
 }
